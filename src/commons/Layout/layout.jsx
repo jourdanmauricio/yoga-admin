021b79836b0a0186472ebbb2f428b8f1b8380232
Mauricio <jourdanmauricio@gmail.com>
2022-11-24 00:31:36 -0300

    Add dashboard

--- src/commons/Layout/layout.jsx
+++ src/commons/Layout/layout.jsx
@@ -1,11 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import {
+  FaCog,
+  FaRegCalendarAlt,
+  FaAngleLeft,
+  FaAngleRight,
+  FaUsers,
+  FaTh,
+} from "react-icons/fa";
+
+import Nav from "../Nav/nav";
+import "./layout.css";
 
 const Layout = (props) => {
+  const [minItems, setMinItems] = useState(false);
+  const handleMinItems = () => {
+    setMinItems(!minItems);
+  };
   return (
     <div className="layout">
-      <nav>nav</nav>
-      <main>{props.children}</main>
-      <footer>FOOTER</footer>
+      <Nav></Nav>
+      <main className="main">
+        <div className={`sidebar ${minItems ? "full" : "mini"}`}>
+          <button className="sidebar__item--cta" onClick={handleMinItems}>
+            {minItems ? <FaAngleRight /> : <FaAngleLeft />}
+          </button>
+          <NavLink className="item__detail" to="#">
+            <FaTh className="material__icon" />
+            <span className="icon-text">Dashboard</span>
+          </NavLink>
+          <br />
+          <NavLink className="item__detail" to="#">
+            <FaCog className="material__icon" />
+            <span className="icon-text">Configuración</span>
+          </NavLink>
+          <br />
+          <NavLink className="item__detail" to="#">
+            <FaRegCalendarAlt className="material__icon" />
+            <span className="icon-text">Clases</span>
+          </NavLink>
+          <br />
+          <NavLink className="item__detail" to="#">
+            <FaUsers className="material__icon" />
+            <span className="icon-text">Alumnos</span>
+          </NavLink>
+        </div>
+        <section
+          className={`main__content ${minItems && "main__content--full"}`}
+        >
+          {props.children}
+        </section>
+      </main>
+      <footer className="footer">FOOTER</footer>
     </div>
   );
 };
---
+++ src/commons/Nav/nav.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { FaPowerOff, FaBars } from "react-icons/fa";
+import logo from "../../assets/logos/logo2.svg";
+
+import { logOut } from "../../store/user";
+
+import "./nav.css";
+
+const Nav = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+  let user = useSelector((state) => state.user.user);
+  let dispatch = useDispatch();
+  let navigate = useNavigate();
+
+  const handleMobileMenu = () => {
+    console.log("Click");
+    setMobileMenu(!mobileMenu);
+  };
+
+  const handleLogout = () => {
+    dispatch(logOut());
+    navigate("/");
+  };
+
+  return (
+    <nav>
+      <div className="menu">
+        <FaBars onClick={handleMobileMenu} />
+      </div>
+      <div className="navbar">
+        <img src={logo} alt="logo" className="logo" />
+        <ul>
+          <li>
+            {user && (
+              <button className="navbar__logout" onClick={handleLogout}>
+                <FaPowerOff />
+              </button>
+            )}
+          </li>
+        </ul>
+      </div>
+      <div
+        className={`mobile-menu ${mobileMenu && "inactive"}`}
+        // className="mobile-menu inactive"
+      >
+        <ul>
+          <li>
+            <a href="/#inicio">Inicio</a>
+          </li>
+          <li>
+            <a href="/#servicios">Servicios</a>
+          </li>
+          <li>
+            <a href="/">Horarios</a>
+          </li>
+          <li>
+            <a href="./contacto.html" className="menu__active">
+              Contacto
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
